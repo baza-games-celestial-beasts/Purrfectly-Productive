ec2e3089bb4cda39b2c09a239f2147c30daf69b2
Vseslav <Vseslav2244@gmail.com>
2023-03-19 01:24:05 +0300

    Generator break and lose logic

--- Assets/Scripts/Generator Logic/Generator.cs
+++ Assets/Scripts/Generator Logic/Generator.cs
@@ -1,13 +1,31 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using Managers.Game_States;
+using Sirenix.OdinInspector;
 using UnityEngine;
+using Zenject;
+using Random = UnityEngine.Random;
 
 namespace Generator_Logic
 {
     public class Generator : MonoBehaviour
     {
         #region Variables
+        [SerializeField] private float breakTime;
+        [Space(10)]
+        [SerializeField] private int brokenItemsToStop;
+        [SerializeField] private int brokenItemsToLose;
+
         private GeneratorItem[] items;
 
+        [ShowInInspector, ReadOnly]
+        private readonly List<GeneratorItem> healthyItems = new();
+        [ShowInInspector, ReadOnly]
+        private List<GeneratorItem> brokenItems = new();
+
+        [Inject] private GameStateManager _gameStateManager;
+        
         public event Action OnBroken;
         public event Action OnFix;
         #endregion
@@ -16,7 +34,42 @@ namespace Generator_Logic
         private void Start()
         {
             items = GetComponentsInChildren<GeneratorItem>();
+            healthyItems.AddRange(items);
+
+            StartCoroutine(BreakItems());
+
+            _gameStateManager.Finish += StopAllCoroutines;
         }
         #endregion
+
+        private IEnumerator BreakItems()
+        {
+            while (true)
+            {
+                yield return new WaitForSeconds(breakTime);
+                BreakItem();
+            }
+            // ReSharper disable once IteratorNeverReturns
+        }
+
+        private void BreakItem()
+        {
+            var targetIndex = Random.Range(0, healthyItems.Count);
+            var targetItem = healthyItems[targetIndex];
+            healthyItems.RemoveAt(targetIndex);
+            
+            brokenItems.Add(targetItem);
+
+            if (brokenItems.Count >= brokenItemsToLose)
+            {
+                _gameStateManager.ChangeState(GameState.Fail);
+                return;
+            }
+
+            if (brokenItems.Count >= brokenItemsToStop)
+            {
+                OnBroken?.Invoke();
+            }
+        }
     }
 }
--- Assets/Scripts/Managers/Game States/GameStateManager.cs
+++ Assets/Scripts/Managers/Game States/GameStateManager.cs
@@ -44,14 +44,16 @@ namespace Managers.Game_States
                 case GameState.Game:
                     StartGame?.Invoke();
                     break;
-                case GameState.Finish:
-                    Finish?.Invoke();
-                    break;
                 case GameState.Victory:
                     Victory?.Invoke();
+                    ChangeState(GameState.Finish);
                     break;
                 case GameState.Fail:
                     Fail?.Invoke();
+                    ChangeState(GameState.Finish);
+                    break;
+                case GameState.Finish:
+                    Finish?.Invoke();
                     break;
                 default:
                     throw new ArgumentOutOfRangeException();
