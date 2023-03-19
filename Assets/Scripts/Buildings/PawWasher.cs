c1b766712e0802c8b93af57f1394b01b154966a0
Vseslav <Vseslav2244@gmail.com>
2023-03-19 12:30:25 +0300

    paws washer, funnel

--- Assets/Scripts/Buildings/PawWasher.cs
+++ Assets/Scripts/Buildings/PawWasher.cs
@@ -1,10 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PawWasher : MonoBehaviour, IInteractable {
-
+public class PawWasher : MonoBehaviour, IInteractable
+{
+    [SerializeField] private float swearTimer = 15f;
     public Vector2 popupPos => transform.position + Vector3.up * 1.2f;
+    
+    private void Start()
+    {
+        Game.inst.player.pawsAreWashed = false;
+    }
 
     public void Interact() {
         Game.inst.player.pawsAreWashed = true;
@@ -12,9 +19,15 @@ public class PawWasher : MonoBehaviour, IInteractable {
 
     public string InteractText() {
         if(Game.inst.player.pawsAreWashed) {
-            return "Лапки уже помыты\n:D";
+            return "Р›Р°РїРєРё СѓР¶Рµ РїРѕРјС‹С‚С‹\n:D";
         } else {
-            return "Помыть лапки [E]";
+            return "РџРѕРјС‹С‚СЊ Р»Р°РїРєРё [E]";
         }
+    }
+
+    private IEnumerator SmearPaws()
+    {
+        yield return new WaitForSeconds(swearTimer);
+        Game.inst.player.pawsAreWashed = false;
     }
 }
